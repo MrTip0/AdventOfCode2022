def3ff2e6255888122d78a199dd70f255775257d
Nicolò Luigi Allegris <77175052+MrTip0@users.noreply.github.com>
2022-12-04 07:30:44 +0100

    day 4

---
+++ d4p2.py
@@ -0,0 +1,17 @@
+f = open("input.txt")
+
+lines = f.readlines()
+r = 0
+
+for line in lines:
+    line = line.strip()
+    pair = line.split(",")
+    ranges = []
+    for range in pair:
+        p = range.split("-")
+        t = (int(p[0]), int(p[1]))
+        ranges.append(t)
+    if (ranges[0][0] >= ranges[1][0] and ranges[0][0] <= ranges[1][1]) or (ranges[0][1] >= ranges[1][0] and ranges[0][1] <= ranges[1][1]) or (ranges[1][0] >= ranges[0][0] and ranges[1][0] <= ranges[0][1]):
+        r += 1
+
+print(r)
