6ab5a95897deddf35a22aab7b5fc53c202ecbe90
Nicolò Luigi Allegris <77175052+MrTip0@users.noreply.github.com>
2022-12-07 17:28:31 +0100

    day 7

---
+++ d7p2.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Node struct {
+	value    int
+	children map[string]*Node
+	parent   *Node
+}
+
+func main() {
+	root := &Node{
+		value:    0,
+		children: make(map[string]*Node, 0),
+		parent:   nil,
+	}
+	ptr := root
+
+	f, _ := os.Open("input.txt")
+	b, _ := ioutil.ReadAll(f)
+
+	for _, v := range strings.Split(string(b), "\n") {
+		if v == "" {
+			continue
+		}
+		if strings.HasPrefix(v, "$ cd ") {
+			v = strings.Split(v, " ")[2]
+			if v == "/" {
+				ptr = root
+			} else if v == ".." {
+				ptr = ptr.parent
+			} else if v != "." {
+				ptr = ptr.children[v]
+			}
+		} else if v != "$ ls" {
+			divided := strings.Split(v, " ")
+			if divided[0] == "dir" {
+				ptr.children[divided[1]] = &Node{
+					value:    0,
+					children: make(map[string]*Node, 0),
+					parent:   ptr,
+				}
+			} else {
+				val, _ := strconv.Atoi(divided[0])
+				ptr.children[divided[1]] = &Node{
+					value:    val,
+					children: nil,
+					parent:   ptr,
+				}
+			}
+		}
+	}
+
+	vals := make([]int, 0)
+	tot := CountValues(&vals, root)
+	r := math.MaxInt
+
+	for _, v := range vals {
+		if v < r && v >= tot-40000000 {
+			r = v
+		}
+	}
+
+	fmt.Printf("%d\n", r)
+}
+
+func CountValues(V *[]int, ptr *Node) int {
+	r := 0
+	for _, val := range ptr.children {
+		if val.children == nil {
+			r += val.value
+		} else {
+			r += CountValues(V, val)
+		}
+	}
+	*V = append(*V, r)
+	return r
+}
