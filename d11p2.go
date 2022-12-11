5aa3a1c03f57ca508a9a26cda5cba47055a46c43
Nicolò Luigi Allegris <nico@allgraf.it>
2022-12-11 11:07:46 +0100

    day 11

---
+++ d11p2.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Monkey struct {
+	Items           *list.List
+	Operation       func(val int) int
+	Module          int
+	IfTrue, IfFalse int
+	Inspected       int
+}
+
+func main() {
+	f, _ := os.Open("input.txt")
+	b, _ := io.ReadAll(f)
+	s := string(b)
+
+	input := strings.Split(s, "\n\n")
+	module := 1
+
+	monkeys := make([]*Monkey, 0, 10)
+	for _, m := range input {
+		if r, e := regexp.MatchString(`^\s*$`, m); r || e != nil {
+			continue
+		}
+		lines := strings.Split(m, "\n")
+		monkey := &Monkey{}
+		monkey.Items = GetItems(lines[1])
+		monkey.Operation = CreateFunction(lines[2])
+		monkey.Module = SplitAndRead(lines[3], "by ")
+		module = lcm(module, monkey.Module)
+		monkey.IfTrue = SplitAndRead(lines[4], "monkey ")
+		monkey.IfFalse = SplitAndRead(lines[5], "monkey ")
+		monkeys = append(monkeys, monkey)
+	}
+
+	for i := 0; i < 10000; i++ {
+		DoRound(monkeys, module)
+	}
+
+	sort.Slice(monkeys, func(i, j int) bool {
+		return monkeys[i].Inspected > monkeys[j].Inspected
+	})
+
+	fmt.Println(monkeys[0].Inspected * monkeys[1].Inspected)
+}
+
+func CreateFunction(description string) func(val int) int {
+	description = strings.Split(description, "= ")[1]
+	tokens := strings.Split(description, " ")
+	var operator func(a, b int) int
+	switch tokens[1] {
+	case "+":
+		operator = func(a, b int) int {
+			return a + b
+		}
+	case "*":
+		operator = func(a, b int) int {
+			return a * b
+		}
+	}
+	return func(val int) int {
+		var a, b int
+		if tokens[0] == "old" {
+			a = val
+		} else {
+			a, _ = strconv.Atoi(tokens[0])
+		}
+		if tokens[2] == "old" {
+			b = val
+		} else {
+			b, _ = strconv.Atoi(tokens[2])
+		}
+		return operator(a, b)
+	}
+}
+
+func GetItems(data string) *list.List {
+	data = strings.Split(data, ": ")[1]
+	elements := strings.Split(data, ", ")
+	r := list.New()
+	for _, e := range elements {
+		v, _ := strconv.Atoi(e)
+		r.PushBack(v)
+	}
+	return r
+}
+
+func SplitAndRead(description, sep string) int {
+	description = strings.Split(description, sep)[1]
+	r, _ := strconv.Atoi(description)
+	return r
+}
+
+func DoRound(V []*Monkey, mod int) {
+	for _, monkey := range V {
+		for {
+			if monkey.Items.Len() == 0 {
+				break
+			}
+			next := monkey.Items.Front()
+			val := next.Value.(int)
+			monkey.Items.Remove(next)
+			val = monkey.Operation(val) % mod
+			if val%monkey.Module == 0 {
+				V[monkey.IfTrue].Items.PushBack(val)
+			} else {
+				V[monkey.IfFalse].Items.PushBack(val)
+			}
+			monkey.Inspected++
+		}
+	}
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
